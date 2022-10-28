19da2bf0c626289ceb97715b8e256f6a8e04bedf
Eric Jangola <eric.jangola-ext@ab-inbev.com>
2022-10-27 21:38:51 -0300

    add see voters

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const candidateOptions = document.getElementById("candidate-options");
 const voteForm = document.getElementById("vote-form");
 
 var proposals = [];
+let voters = [];
 var myAddress;
 var eleicao;
 // const CONTRACT_ADDRESS = "0xf396a27e87ccf377c289b8375c3817046e94133e";
@@ -152,4 +153,83 @@ $("#btnFinish").on('click', function () {
 			console.log(error.message);
 			return;
 		});
+});
+
+$("#btnDelegate").on('click', function () {
+
+	const address = $("#delegate-input").val();
+
+	eleicao.methods.delegate(address).send({ from: myAddress })
+		.on('receipt', function (receipt) {
+			//getCandidatos(eleicao, populaCandidatos);
+			windows.location.reaload(true);
+		})
+		.on('error', function (error) {
+			console.log(error.message);
+			return;
+		});
+});
+
+function chairperson() {
+
+	eleicao.methods.chairperson().call({ from: myAddress })
+		.on('receipt', async function (error, data) {
+			//getCandidatos(eleicao, populaCandidatos);
+			return data;
+		})
+		.on('error', function (error) {
+			console.log(error.message);
+			return;
+		});
+}
+
+
+function populaVoters(voters) {
+	voters.forEach((voter, index) => {
+		// Creates a row element.
+		const rowElem = document.createElement("tr");
+
+		// Creates a cell element for the name.
+		const nameCell = document.createElement("td");
+		nameCell.innerText = voter.name;
+		rowElem.appendChild(nameCell);
+
+		// Creates a cell element for the votes.
+		const voteCell = document.createElement("td");
+		voteCell.id = "vote-" + voter.name;
+		voteCell.innerText = voter.status;
+		rowElem.appendChild(voteCell);
+
+		// Adds the new row to the voting table.
+		tableElem.appendChild(rowElem);
+
+		// Creates an option for each candidate
+		const candidateOption = document.createElement("option");
+		candidateOption.value = index;
+		candidateOption.innerText = voter.name;
+		candidateOptions.appendChild(candidateOption);
+	});
+
+	$("#table-body-eleitor").show();
+}
+
+$("#btnSeeVoters").on('click', function () {
+	contractRef.methods.getVotersCount().call(async function (error, count) {
+		console.log('count', count);
+		for (i = 0; i < count; i++) {
+			try {
+				await contractRef.methods.getVoterFromId(i).call().then((data) => {
+					var voter = {
+						name: data[0],
+						status: data[1]
+					};
+					voters.push(voter);
+				});
+			} catch (err) {
+				console.log(err);
+			}
+		}
+		
+		populaVoters(voters);
+	});
 });
