8e3eea9cb936389b514ddacfc666b7e25836a161
Eric Jangola <eric.jangola-ext@ab-inbev.com>
2022-10-27 21:21:07 -0300

    add finish

--- js/app.js
+++ js/app.js
@@ -11,11 +11,11 @@ const CONTRACT_ADDRESS = "0x1fF486CD5C7CCe331d7B00D07D3ED21A83DDeef4";
 
 const ethEnabled = () => {
 	if (window.ethereum) {
-    		window.web3 = new Web3(window.ethereum);
-    		window.ethereum.enable();
-    		return true;
-  	}
-  	return false;
+		window.web3 = new Web3(window.ethereum);
+		window.ethereum.enable();
+		return true;
+	}
+	return false;
 }
 
 const getMyAccounts = accounts => {
@@ -28,15 +28,15 @@ const getMyAccounts = accounts => {
 				console.log(myAddress + " : " + await window.web3.eth.getBalance(myAddress));
 			});
 		}
-	} catch(error) {
+	} catch (error) {
 		console.log("Erro ao obter contas...");
 	}
 };
 
-window.addEventListener('load', async function() {
+window.addEventListener('load', async function () {
 
 	if (!ethEnabled()) {
-  		alert("Por favor, instale um navegador compatível com Ethereum ou uma extensão como o MetaMask para utilizar esse dApp!");
+		alert("Por favor, instale um navegador compatível com Ethereum ou uma extensão como o MetaMask para utilizar esse dApp!");
 	}
 	else {
 		getMyAccounts(await web3.eth.getAccounts());
@@ -46,20 +46,19 @@ window.addEventListener('load', async function() {
 	}
 });
 
-function getCandidatos(contractRef,callback)
-{
+function getCandidatos(contractRef, callback) {
 	//contractRef.methods.getProposalsCount().call().then((count)=>{
 	contractRef.methods.getProposalsCount().call(async function (error, count) {
 		console.log('count', count);
-		for (i=0; i<count; i++) {
-			await contractRef.methods.getProposal(i).call().then((data)=>{
+		for (i = 0; i < count; i++) {
+			await contractRef.methods.getProposal(i).call().then((data) => {
 				console.log('data', data);
 				var proposal = {
-          				name : data[0],
-          				voteCount : data[1]
-      				};
+					name: data[0],
+					voteCount: data[1]
+				};
 				proposals.push(proposal);
- 			});
+			});
 		}
 		if (callback) {
 			callback(proposals);
@@ -80,7 +79,7 @@ function populaCandidatos(candidatos) {
 
 		// Creates a cell element for the votes.
 		const voteCell = document.createElement("td");
-		voteCell.id = "vote-" + candidato.name; 
+		voteCell.id = "vote-" + candidato.name;
 		voteCell.innerText = candidato.voteCount;
 		rowElem.appendChild(voteCell);
 
@@ -92,51 +91,65 @@ function populaCandidatos(candidatos) {
 		candidateOption.value = index;
 		candidateOption.innerText = candidato.name;
 		candidateOptions.appendChild(candidateOption);
-        });
+	});
 }
 
 
-$("#btnVote").on('click',function(){
+$("#btnVote").on('click', function () {
 	candidato = $("#candidate-options").children("option:selected").val();
 
-        eleicao.methods.vote(candidato).send({from: myAddress})
-	       .on('receipt',function(receipt) {
+	eleicao.methods.vote(candidato).send({ from: myAddress })
+		.on('receipt', function (receipt) {
 			//getCandidatos(eleicao, populaCandidatos);
 			windows.location.reaload(true);
 		})
-		.on('error',function(error) {
+		.on('error', function (error) {
 			console.log(error.message);
-               		return;     
-        	});  
+			return;
+		});
 
 });
 
-$("#btnAddCandidate").on('click',function(){
+$("#btnAddCandidate").on('click', function () {
 	const candidato = $("#candidate-name").val();
 
-        eleicao.methods.addProposalToList([candidato]).send({from: myAddress})
-	       .on('receipt',function(receipt) {
+	eleicao.methods.addProposalToList([candidato]).send({ from: myAddress })
+		.on('receipt', function (receipt) {
 			//getCandidatos(eleicao, populaCandidatos);
 			windows.location.reaload(true);
 		})
-		.on('error',function(error) {
+		.on('error', function (error) {
 			console.log(error.message);
-               		return;     
-        	});  
+			return;
+		});
 });
 
 
-$("#btnAddVotingPerson").on('click',function(){
+$("#btnAddVotingPerson").on('click', function () {
 	const address = $("#voting-person").val();
 	const contractName = $("#voting-person-name").val();
 
-        eleicao.methods.giveRightToVote(address, contractName).send({from: myAddress})
-	       .on('receipt',function(receipt) {
+	eleicao.methods.giveRightToVote(address, contractName).send({ from: myAddress })
+		.on('receipt', function (receipt) {
 			//getCandidatos(eleicao, populaCandidatos);
 			windows.location.reaload(true);
 		})
-		.on('error',function(error) {
+		.on('error', function (error) {
 			console.log(error.message);
-               		return;     
-        	});  
+			return;
+		});
+});
+
+$("#btnFinish").on('click', function () {
+	eleicao.methods.finishVoting().send({ from: myAddress })
+		.on('receipt', function (receipt) {
+
+			$("#table-body").show();
+			//getCandidatos(eleicao, populaCandidatos);
+			windows.location.reaload(true);
+		})
+		.on('error', function (error) {
+			console.log(error.message);
+			return;
+		});
 });
